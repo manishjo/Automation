49e078576dc266389f060978b29d2e8ae79f2558
surajbab <https://github.com/surajbabar>
2014-04-14 23:04:00 +0530

    used form element instead of button

--- facebook_automation/fbMsgAutomation.js
+++ facebook_automation/fbMsgAutomation.js
@@ -2,14 +2,22 @@
 var page = require('webpage').create();
 
 var fillLoginInfo = function(){
-    document.getElementById('email').value = "joshimaneesh26@gmail.com";
-    document.getElementById('pass').value = "123456";
+  	var frm = document.getElementById("login_form");
+    frm.elements["email"].value = 'joshimaneesh26@gmail.com';
+    frm.elements["pass"].value = '12345';
+    frm.submit();
 }
 
-var doLogin = function(){
-    page.evaluate(fillLoginInfo);
-    page.render('./screens/img.png');
+page.onLoadFinished = function(){
+	if(page.title == "Welcome to Facebook - Log In, Sign Up or Learn More"){
+		page.evaluate(fillLoginInfo);
+		return;
+	}
+	else
+		page.render('./screens/some.png');
+	console.log(page.title);
     console.log("1st step completed");
+    phantom.exit();
 }
 
-page.open('https://www.facebook.com/',doLogin);
+page.open('https://www.facebook.com/');
